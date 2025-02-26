e9497ea4921d8cd80f01ae79273ee8660eefd64b
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-02-26 22:03:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -206,15 +206,16 @@ async def not_joined(client: Client, message: Message):
 
     if FORCE_PIC:  # Check if FORCE_PIC has a value
         await message.reply_photo(
-            photo=FORCE_PIC,
-            caption=FORCE_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
+            photo = random.choice(PICS),
+            caption = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
             ),
-            reply_markup=InlineKeyboardMarkup(buttons)
+            reply_markup =InlineKeyboardMarkup(buttons)
+	        message_effect_id=5104841245755180586 #🔥
         )
     else:
         await message.reply(
