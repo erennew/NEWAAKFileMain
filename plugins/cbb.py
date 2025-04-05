14c21384bd20504c11e9d30bbef5ff58e83acb8e
erennew <erenyeagermikasa84@gmail.com>
2025-04-05 18:54:30 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,13 +10,19 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b><blockquote>○ ᴏᴡɴᴇʀ : <a href='@CulturedTeluguWeebBot'>ʀᴀᴠɪ</a>\n○ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/CulturedTeluguweeb'>ᴄᴜʟᴛᴜʀᴇᴅ ᴡᴇᴇʙ</a>\n○ ᴏɴɢᴏɪɴɢ : <a href='https://t.me/+BiVvkpD5ieIxZTNl'>ᴄᴛᴡ ᴏɴɢᴏɪɴɢ</a>\n○ ᴅɪꜱᴄᴜꜱꜱ ɢʀᴏᴜᴘ : <a href='https://t.me/+IIgB6RgivTI2NzA1'>ᴄᴜʟᴛᴜʀᴇᴅ ᴡᴇᴇʙꜱ
-</a></blockquote></b>",
+            text = (
+                "<b><blockquote>☠️ LUFFY’S MAP ☠️\n\n"
+                "👒 ᴄᴀᴘᴛᴀɪɴ : <a href='https://t.me/CulturedTeluguWeebBot'>ʀᴀᴠɪ ᴅᴏɴ!</a>\n"
+                "🏴‍☠️ ᴍᴀɪɴ ɪꜱʟᴀɴᴅ : <a href='https://t.me/CulturedTeluguweeb'>ᴄᴜʟᴛᴜʀᴇᴅ ᴡᴇᴇʙ</a>\n"
+                "📺 ᴄᴜʀʀᴇɴᴛ ᴀɴɪᴍᴇ ᴍɪssɪᴏɴ : <a href='https://t.me/+BiVvkpD5ieIxZTNl'>ᴄᴛᴡ ᴏɴɢᴏɪɴɢ</a>\n"
+                "🗣️ ᴘɪʀᴀᴛᴇ ᴛᴀʟᴋs : <a href='https://t.me/+IIgB6RgivTI2NzA1'>ᴄᴜʟᴛᴜʀᴇᴅ ᴡᴇᴇʙꜱ</a>"
+                "</blockquote></b>"
+            ),
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("⚡ Cℓσѕє", callback_data = "close")
+                        InlineKeyboardButton("🛑 Close Map", callback_data = "close")
                     ]
                 ]
             )
