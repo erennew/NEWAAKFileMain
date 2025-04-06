3d394fc17002a1a5e9f97bb9317edd24cb7aa5e9
erennew <erenyeagermikasa84@gmail.com>
2025-04-07 03:36:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from bot import Bot
 import asyncio
+from bot import bot
 
 async def main():
-    bot = Bot()
-    await bot.start()
     await bot.run()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
